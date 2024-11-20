20eda687f2c7480cd1d69e5ebfefc94ad6323203
Ender Calderón <177250779+Yohebyth@users.noreply.github.com>
2024-11-20 21:18:14 +0000

    correccion en el map

--- src/js/component/todolist.jsx
+++ src/js/component/todolist.jsx
@@ -26,18 +26,23 @@ const TodoList = () => {
                 <input className="input" type="text" placeholder="What needs to be done? " onKeyDown={handleOnChange} />
                 <ul>
                     {
-                        need.map((needParam) => (
-                        <li key={needParam}>
-                            {needParam}
-                            <button
-                                className="delete"
-                                onClick={() => {deleteNeed(needParam);}}
-                            ><i className="fa-solid fa-x" /></button>
-                        </li>                        
-                    ))}
+                        need.map((needParam) => {
+                        return(
+                            <li key={needParam}>
+                                {needParam}
+                                <button
+                                    className="delete"
+                                    onClick={() => {deleteNeed(needParam);}}
+                                >
+                                    <i className="fa-solid fa-x" />
+                                </button>
+                            </li>   
+                        )                     
+                    })}
                 </ul>
-                <p>{need.length} {need.length > 1 ? "items left" : "item left"}</p>
+                <p>{need.length} {need.length > 1 ? "items left" : "item left"}</p>                
             </div>
+            <button type="button" className="btn btn-secondary">Delete All</button>
         </div>
     )
 }
